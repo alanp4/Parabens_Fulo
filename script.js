9e10d84361ba084c3943bda2602ca214dd1c5fc3
alanp4 <72811434+alanp4@users.noreply.github.com>
2024-02-11 14:32:18 -0300

    Arquivos

---
+++ script.js
@@ -0,0 +1,10 @@
+function magia(){
+  var img1 = document.getElementById('img-dedo1')
+  var img2 = document.getElementById('img-dedo2')
+  document.getElementById("h2-amigos").innerHTML="SOMOS AMIGOS ATÉ...O FIM ! "
+  img1.style.marginLeft='80px'
+  img1.style.transition='all 0.7s'
+  img2.style.position='absolute'
+  img2.style.marginLeft='170px'
+  
+}
